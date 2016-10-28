5a4e56d415702254cd2840b75706247115ee9985
Hoobie <hoobie92@gmail.com>
2016-10-28 10:54:10 +0200

    Extract function

--- server/src/main/java/pl/edu/agh/Server.java
+++ server/src/main/java/pl/edu/agh/Server.java
@@ -3,22 +3,26 @@ package pl.edu.agh;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.logging.LogLevel;
 import io.reactivex.netty.protocol.tcp.server.TcpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
 
 public class Server {
 
-    public static void main(final String[] args) {
-        TcpServer<ByteBuf, ByteBuf> server;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
-        server = TcpServer.newServer(12345)
+    public static void main(final String[] args) {
+        createServer(Integer.parseInt(args[0])).awaitShutdown();
+    }
+
+    private static TcpServer<ByteBuf, ByteBuf> createServer(int port) {
+        return TcpServer.newServer(port)
                 .enableWireLogging("server", LogLevel.DEBUG)
                 .start(connection -> connection
                         .writeStringAndFlushOnEach(connection.getInput()
                                 .map(bb -> bb.toString(Charset.defaultCharset()))
-                                .doOnNext(System.out::println)
+                                .doOnNext(LOGGER::info)
                                 .map(msg -> "echo => " + msg)));
-
-        server.awaitShutdown();
     }
 }
