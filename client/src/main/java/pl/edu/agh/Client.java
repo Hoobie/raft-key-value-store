dec419eb446414399168485b94a5938216a7b4d6
Hoobie <hoobie92@gmail.com>
2016-10-29 00:57:28 +0200

    Improve client

--- client/src/main/java/pl/edu/agh/Client.java
+++ client/src/main/java/pl/edu/agh/Client.java
@@ -3,6 +3,8 @@ package pl.edu.agh;
 import io.netty.buffer.ByteBuf;
 import io.netty.handler.logging.LogLevel;
 import io.reactivex.netty.protocol.tcp.client.TcpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rx.Observable;
 
 import java.net.InetSocketAddress;
@@ -11,20 +13,17 @@ import java.nio.charset.Charset;
 
 public class Client {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Client.class);
+
     public static void main(String[] args) {
         SocketAddress serverAddress = new InetSocketAddress(12345);
 
         TcpClient.newClient(serverAddress)
                 .enableWireLogging("client", LogLevel.DEBUG)
                 .createConnectionRequest()
-                .flatMap(connection ->
-                        connection.writeString(Observable.just("Hello World!"))
-                                .cast(ByteBuf.class)
-                                .concatWith(connection.getInput())
-                )
-                .take(1)
-                .map(bb -> bb.toString(Charset.defaultCharset()))
+                .flatMap(connection -> connection.writeString(Observable.just("Hello World!")).cast(ByteBuf.class))
+                .map(b -> b.toString(Charset.defaultCharset()))
                 .toBlocking()
-                .forEach(System.out::println);
+                .forEach(LOGGER::info);
     }
 }
