432239c162724ac15c121a3fff905cfc1e38f3aa
FleenMobile\Andrzej <fleenmobile@gmail.com>
2016-11-13 15:17:38 +0100

    Wrap LogEntry in AppendEntries (for unification with problem description)

--- common/src/main/java/pl/edu/agh/messages/replication/AppendEntries.java
+++ common/src/main/java/pl/edu/agh/messages/replication/AppendEntries.java
@@ -5,8 +5,18 @@ import pl.edu.agh.messages.RaftMessage;
 
 public class AppendEntries implements RaftMessage {
     public int term;
+    public LogEntry logEntry;
 
+    // Heartbeat entry
     public AppendEntries(int term) {
         this.term = term;
+    }
+
+    public AppendEntries(LogEntry logEntry) {
+        this.logEntry = logEntry;
+    }
+
+    public LogEntry getLogEntry() {
+        return logEntry;
     }
 }
